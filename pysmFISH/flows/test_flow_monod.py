08908b8136af3f6b24059f3e0f63f94dba8054b0
simone codeluppi <simone.codeluppi@gmail.com>
2020-08-31 14:11:23 +0200

    created test flow condor

---
+++ pysmFISH/flows/test_flow_monod.py
@@ -0,0 +1,47 @@
+import prefect
+import time
+from prefect import task, Flow, Parameter, flatten, unmapped
+from prefect.engine.executors import DaskExecutor
+
+from pysmFISH.dask_cluster_utilities_tasks import htcondor_cluster_setup
+
+
+# testing import
+from pathlib import Path
+
+
+@task
+def test_parallel(a):
+    print('test_parallel')
+    time.sleep(200)
+
+
+if __name__ == '__main__':
+
+    monod_cluster = {
+            'cores': 1,
+            'memory': '2GB',
+            'disk': '0.1GB',
+            'local_directory': '/tmp'      
+        }
+
+    cluster = htcondor_cluster_setup(monod_cluster)
+
+    # with Flow("test_running",schedule=schedule) as flow:
+    with Flow("test_running") as flow:
+
+        a = list(range(20))
+        test_parallel.map(a)
+
+
+    executor = DaskExecutor(address=cluster.scheduler_address)
+    # with raise_on_exception():
+
+    flow_state = flow.run(executor=executor)
+    # flow_state = flow.run(executor=executor)
+    
+    flow.visualize(flow_state=flow_state)
+    
+
+
+
