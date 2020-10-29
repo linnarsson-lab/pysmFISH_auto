74574addfc5062e255ffdbf827bdde39794abec9
simone codeluppi <simone.codeluppi@gmail.com>
2020-10-29 14:22:07 +0100

    defined cluster size

--- pysmFISH/dask_cluster_utilities_tasks.py
+++ pysmFISH/dask_cluster_utilities_tasks.py
@@ -82,8 +82,8 @@ def start_processing_env(processing_env_config:Dict,experiment_info:Dict):
         experiment_type = experiment_info['Experiment_type']
         cluster_config_parameters = processing_env_config[processing_engine][experiment_type]
         cluster = htcondor_cluster_setup(cluster_config_parameters)
-        # cluster.scale(jobs=100)
-        cluster.adapt(minimum_jobs=32)
+        cluster.scale(jobs=150)
+        # cluster.adapt(minimum_jobs=32)
         return cluster
     elif processing_engine == 'local':
         cluster = local_cluster_setup()
