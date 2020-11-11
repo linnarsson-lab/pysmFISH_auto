b985b2158b96d0c5bd24bd852d6b35cbb9fe693a
Simone Codeluppi <simone@Simones-MacBook-Pro.local>
2020-11-11 19:40:40 +0100

    test move context

--- pysmFISH/TestLogger_flow.py
+++ pysmFISH/TestLogger_flow.py
@@ -4,6 +4,7 @@ from prefect.engine.executors import DaskExecutor
 from prefect.utilities.debug import raise_on_exception
 from datetime import timedelta, datetime
 from prefect.schedules import IntervalSchedule
+from prefect import context
 
 from pythonjsonlogger import jsonlogger
 
@@ -24,7 +25,7 @@ import logging
 
 @task(task_run_name=lambda **kwargs: f"testing-logger-writing-logs-{kwargs['x']}-suiname")
 def wlog(x):
-    from prefect import context
+    
     logger = context.get("logger")
     logger.debug('la culara')
     # logger = prefect_logging_setup('test')
@@ -36,9 +37,9 @@ def wlog(x):
 a = list(range(10))
 # with Flow("test_running",schedule=schedule) as flow:
 with Flow("logging-flow",environment=LocalEnvironment(DaskExecutor(address='tcp://193.10.16.58:18938'))) as flow:
-    logger = prefect.utilities.logging.get_logger()
-    logger.info('this log is generated in the flow')
+    # logger = prefect.utilities.logging.get_logger()
+    # logger.info('this log is generated in the flow')
     out_task = wlog.map(a)
-    logger.info('done')
+    # logger.info('done')
 
 flow.register(project_name="test")
