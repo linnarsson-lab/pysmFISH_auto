708716130e6c6e1459068c613f5c990df4772dfb
Simone Codeluppi <simone@Simones-MacBook-Pro.local>
2020-11-11 14:44:06 +0100

    changed position

--- pysmFISH/TestLogger_flow.py
+++ pysmFISH/TestLogger_flow.py
@@ -32,10 +32,10 @@ def wlog(x):
 
 if __name__ == '__main__':
 
+    logger = prefect.utilities.logging.get_logger()
     a = list(range(10))
     # with Flow("test_running",schedule=schedule) as flow:
     with Flow("logging-flow",environment=LocalEnvironment(DaskExecutor(address='tcp://193.10.16.58:18938'))) as flow:
-        logger = prefect.utilities.logging.get_logger()
         logger.info('this log is generated in the flow')
         c = setup_extra_loggers()
         out_task = wlog.map(a)
