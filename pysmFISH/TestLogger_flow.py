dff8f5f20e52106dc9708bc538165c1f12718731
Simone Codeluppi <simone@uf-m8cg53624gn.user.ki.se>
2020-11-13 13:13:54 +0100

    changed IP cluster

--- pysmFISH/TestLogger_flow.py
+++ pysmFISH/TestLogger_flow.py
@@ -23,7 +23,7 @@ def wlog(x):
     logger.info(f'done sleep')
 
 a = list(range(10))
-with Flow("logging-flow",environment=LocalEnvironment(DaskExecutor(address='tcp://193.10.16.58:18938'))) as flow:
+with Flow("logging-flow",environment=LocalEnvironment(DaskExecutor(address='tcp://193.10.16.58:14211'))) as flow:
     # logger = prefect.utilities.logging.get_logger()
     # logger.info('this log is generated in the flow')
     out_task = wlog.map(a)
--- pysmFISH/test_presence.py
+++ pysmFISH/test_presence.py
@@ -1,7 +1,6 @@
 from logger_utils import function_logger
 
+
 from dots_calling import osmFISH_dots_thr_selection
 
-logger = function_logger()
-
-logger.info('cane')
+print('loaded everything no problems')
