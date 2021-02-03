00e7aad839798f6da1ba34a5642bf01e91f82951
simone codeluppi <simone.codeluppi@gmail.com>
2021-02-03 13:13:38 +0100

    added visualization cli fun

--- cli/cli.py
+++ cli/cli.py
@@ -1,12 +1,14 @@
 import click
 
 from cli_tranfer_files.commands import data_transfer
+from cli_viz import vizcounts
 
 @click.group()
 def pysmFISH_cli():
     pass
 
 pysmFISH_cli.add_command(data_transfer)
+pysmFISH_cli.add_command(vizcounts)
 
 if __name__ == '__main__':
     pysmFISH_cli()
