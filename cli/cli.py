ec514d5f8049caf4c579b53ec7053f7f3d4b885a
simone codeluppi <simone.codeluppi@gmail.com>
2021-02-24 12:43:37 +0100

    added cli cenv creation

--- cli/cli.py
+++ cli/cli.py
@@ -2,6 +2,7 @@ import click
 
 from cli_viz.commands import viz_utilities
 from cli_data_org.commands import data_organization_utilities
+from cli_prep.commands import setup_processing_env
 
 @click.group()
 def pysmFISH_cli():
@@ -9,6 +10,7 @@ def pysmFISH_cli():
 
 pysmFISH_cli.add_command(viz_utilities)
 pysmFISH_cli.add_command(data_organization_utilities)
+pysmFISH_cli.add_command(setup_processing_env)
 
 if __name__ == '__main__':
     pysmFISH_cli()
