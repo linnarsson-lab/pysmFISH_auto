e1f3845a4babc14a1685529310a3859108bf49f1
simone codeluppi <simone.codeluppi@gmail.com>
2021-03-05 11:56:42 +0100

    activated filt

--- flows/general_flow_single_fov.py
+++ flows/general_flow_single_fov.py
@@ -296,7 +296,7 @@ for fov,sorted_grp in sorted_grps.items():
 
     all_futures.append(future)
 
-# _ = client.gather(all_futures)
+_ = client.gather(all_futures)
 # tracebacks = {}
 # for future in as_completed(all_futures):
 #     logger_print.info(f'processed {future.key} in {time.time()-start} sec')
