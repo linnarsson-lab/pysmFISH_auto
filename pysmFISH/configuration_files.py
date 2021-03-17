33bcfeda61dc4e2ca816e5c2f1f5a615d8eeda48
simone codeluppi <simone.codeluppi@gmail.com>
2021-03-17 15:11:57 +0100

    changed function for processing

--- pysmFISH/configuration_files.py
+++ pysmFISH/configuration_files.py
@@ -345,12 +345,12 @@ def create_function_runner(experiment_fpath,experiment_info):
     experiment_fpath = Path(experiment_fpath)
     running_functions = OrderedDict()
 
-    running_type = experiment_info['Sample']
+    running_type = experiment_info['Pipeline']
     stitching_type = experiment_info['Stitching_type']
 
     if running_type == 'eel-human-GBM':
         running_functions = { 'fish_channels_preprocessing':'filter_remove_large_objs',
-                            'fish_channels_dots_calling':'osmFISH_peak_based_detection_test',
+                            'fish_channels_dots_calling':'osmFISH_barcoded_peak_based_detection_masked_thr_test',
                             'reference_channels_dots_calling': 'osmFISH_peak_based_detection_test',
                             'registration_reference':'calculate_shift_hybridization_fov_test',
                             'registration_fish': 'register_fish_test',
@@ -361,7 +361,7 @@ def create_function_runner(experiment_fpath,experiment_info):
         logger.info(f'selected functions for {running_type}')
         pass
 
-    elif running_type == 'human-embryo':
+    elif running_type == 'eel-human-embryo':
         running_functions = { 'fish_channels_preprocessing':'standard_not_norm_preprocessing',
                             'fish_channels_dots_calling':'osmFISH_peak_based_detection_test',
                             'reference_channels_dots_calling': 'osmFISH_peak_based_detection_test',
