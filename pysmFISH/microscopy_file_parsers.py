b8c40c5f0d58a77f0f35208ddf2f2f551362f0e4
simone codeluppi <simone.codeluppi@gmail.com>
2021-04-03 13:06:52 +0200

    adjusted nc

--- pysmFISH/microscopy_file_parsers.py
+++ pysmFISH/microscopy_file_parsers.py
@@ -531,11 +531,11 @@ def nikon_nd2_autoparser_xarray_zarr(nd2_file_path, parsed_raw_data_fpath, exper
                                   )
             data_xr = data_xr.chunk(chunks_dict)
             data_dset_name = tag_name + '_fov_' + str(fov)
-            data_dset_path = (parsed_raw_data_fpath / (data_dset_name + '.zarr')).as_posix()
+            data_dset_path = (parsed_raw_data_fpath / (data_dset_name + '.nc')).as_posix()
 #             data_dset = xr.Dataset({data_dset_name: data_xr})
             data_dset = xr.Dataset({'raw_data': data_xr},attrs=attrs)
-            data_dset.to_zarr(data_dset_path,mode='w')
-#             data_dset.to_netcdf(parsed_raw_data_fpath + '/' + data_dset_name + '.nc')
+            # data_dset.to_zarr(data_dset_path,mode='w')
+            data_dset.to_netcdf(data_dset_path,mode='w')
 
         # Rename the nd2 files
         new_file_name = tag_name + '.nd2'
@@ -708,7 +708,7 @@ def nikon_nd2_reparser_xarray_zarr(nd2_file_path,parsed_raw_data_fpath,experimen
             data_dset_path = (parsed_raw_data_fpath / (data_dset_name + '.nc')).as_posix()
 #             data_dset = xr.Dataset({data_dset_name: data_xr})
             data_dset = xr.Dataset({'raw_data': data_xr},attrs=attrs)
-            data_dset.to_zarr(data_dset_path,mode='w')
+            # data_dset.to_zarr(data_dset_path,mode='w')
             data_dset.to_netcdf(data_dset_path,mode='w')
 
 
