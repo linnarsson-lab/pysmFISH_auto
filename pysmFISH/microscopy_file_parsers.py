1c30dd2f3e48393fb060e20e9379f1308d9ebb9a
simone codeluppi <simone.codeluppi@gmail.com>
2021-04-03 13:46:57 +0200

    define store

--- pysmFISH/microscopy_file_parsers.py
+++ pysmFISH/microscopy_file_parsers.py
@@ -708,7 +708,7 @@ def nikon_nd2_reparser_xarray_zarr(nd2_file_path,parsed_raw_data_fpath,experimen
             data_dset_path = (parsed_raw_data_fpath / (data_dset_name + '.nc')).as_posix()
 #             data_dset = xr.Dataset({data_dset_name: data_xr})
             data_dset = xr.Dataset({'raw_data': data_xr},attrs=attrs)
-            # data_dset.to_zarr(chunk_store=data_dset_path,mode='w')
+            data_dset.to_zarr(store=data_dset_path,mode='w')
             # data_dset.to_netcdf(data_dset_path,mode='w')
 
 
