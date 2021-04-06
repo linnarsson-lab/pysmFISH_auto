9f0ad92b68cd62c59f23c26b5331ede288a4264c
simone codeluppi <simone.codeluppi@gmail.com>
2021-04-06 06:55:04 +0200

    changed to dask compute

--- flows/general_flow_single_fov.py
+++ flows/general_flow_single_fov.py
@@ -284,10 +284,10 @@ for index_value, fov_subdataset in all_imgs_fov.iterrows():
     #                                 save_steps_output=False)
     
     all_futures_filtering_counting.append(future)
-print(len(all_futures_filtering_counting))
+
 
 d = dask.delayed(cane)(all_futures_filtering_counting)
-z = client.compute(d)
+z = dask.compute(d)
 # _ = client.gather(all_futures_filtering_counting)
 
 logger.info(f'preprocessing and dots counting completed in {(time.time()-start)/60} min')
