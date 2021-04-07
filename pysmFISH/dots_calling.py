5e4d68f8f1a16df3ce9f1235e8dacbdc1930e0c9
simone codeluppi <simone.codeluppi@gmail.com>
2021-04-07 07:31:17 +0200

    error

--- pysmFISH/dots_calling.py
+++ pysmFISH/dots_calling.py
@@ -603,7 +603,8 @@ def osmFISH_peak_based_detection_fast(img:np.ndarray,
                 counts_dict['dot_id'] = dot_id_array
                 counts_dict['dot_intensity'] = dots.intensity_array
                 counts_dict['selected_thr'] = thr_array
-
+                
+                counts_df = pd.DataFrame(counts_dict)
                 fov_subdataset_df = pd.concat([fov_subdataset_df]*counts_df.shape[0],axis=0).sort_index().reset_index(drop=True)
             
             else:
