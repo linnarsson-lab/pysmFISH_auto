d3f08ad2ca87995acf055e6d9426656d3cbfb6c9
simone codeluppi <simone.codeluppi@gmail.com>
2021-04-09 17:44:29 +0200

    import

--- pysmFISH/toy_example.py
+++ pysmFISH/toy_example.py
@@ -1,8 +1,9 @@
 from dask import distributed, Client
 from pysmFISH.logger_utils import selected_logger
+import time
 
 logger = selected_logger()
 
 client= Client()
 logger.debug(f'dask client: {client}')
-
+time.sleep(500)
