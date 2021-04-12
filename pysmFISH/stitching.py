417d2d3f5b891bebc04be6028689c3025ccf3cef
simone codeluppi <simone.codeluppi@gmail.com>
2021-04-12 11:35:52 +0200

    adjusted stitching

--- pysmFISH/stitching.py
+++ pysmFISH/stitching.py
@@ -353,13 +353,13 @@ def stitch_using_microscope_fov_coords_test(decoded_df,fov,tile_corners_coords_p
 
 
 def stitch_using_microscope_fov_coords_new(decoded_df,tile_corners_coords_pxl):
-    fov = decoded_df.iloc[0]['fov_num']
-    r_microscope_coords = tile_corners_coords_pxl[fov,0]
-    c_microscope_coords = tile_corners_coords_pxl[fov,1]
     if decoded_df['r_px_registered'].isnull().values.all():
         decoded_df['r_px_microscope_stitched'] = np.nan
         decoded_df['c_px_microscope_stitched'] = np.nan
     else:
+        fov = decoded_df.iloc[0]['fov_num']
+        r_microscope_coords = tile_corners_coords_pxl[fov,0]
+        c_microscope_coords = tile_corners_coords_pxl[fov,1]
         decoded_df['r_px_microscope_stitched'] =  r_microscope_coords - decoded_df['r_px_registered']
         decoded_df['c_px_microscope_stitched'] =  c_microscope_coords - decoded_df['c_px_registered']
 
