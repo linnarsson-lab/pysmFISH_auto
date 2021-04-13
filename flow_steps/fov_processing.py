3fca095e4351f3cb2603dcbb18da36f6985ee8b4
simone codeluppi <simone.codeluppi@gmail.com>
2021-04-13 16:22:16 +0200

    typo

--- flow_steps/fov_processing.py
+++ flow_steps/fov_processing.py
@@ -124,6 +124,104 @@ def single_fov_round_processing_eel(fov_subdataset,
     return counts
 
 
+def single_fov_round_processing_serial(fov_subdataset,
+                                   analysis_parameters,
+                                   running_functions,
+                                   dark_img,
+                                   experiment_fpath,
+                                   save_steps_output=False):
+
+    logger = selected_logger()
+    experiment_fpath = Path(experiment_fpath)
+
+    # Path of directory where to save the intermediate results
+    filtered_img_path = experiment_fpath / 'tmp' / 'filtered_images'
+    raw_counts_path = experiment_fpath / 'tmp' / 'raw_counts'
+    
+    experiment_name = fov_subdataset.experiment_name
+    pipeline = fov_subdataset.pipeline
+    processing_type = fov_subdataset.processing_type
+    zarr_grp_name = fov_subdataset.grp_name
+    
+    raw_data_location = Path(fov_subdataset.raw_data_location)
+    parsed_raw_data_fpath = raw_data_location.parent
+
+    if processing_type == 'fish':
+        
+        processing_parameters = analysis_parameters['fish']
+        if running_functions['fish_channels_preprocessing'] == 'filter_remove_large_objs':
+
+            img, masked_img = getattr(pysmFISH.preprocessing,running_functions['fish_channels_preprocessing'])(
+                                                            zarr_grp_name,
+                                                            parsed_raw_data_fpath,
+                                                            processing_parameters,
+                                                            dark_img)
+
+            counts = getattr(pysmFISH.dots_calling,running_functions['fish_channels_dots_calling'])(
+                                                                masked_img,
+                                                                fov_subdataset,
+                                                                processing_parameters)                                              
+
+        else:
+
+            img = getattr(pysmFISH.preprocessing,running_functions['fish_channels_preprocessing'])(
+                                                            zarr_grp_name,
+                                                            parsed_raw_data_fpath,
+                                                            processing_parameters,
+                                                            dark_img)
+
+
+            counts = getattr(pysmFISH.dots_calling,running_functions['fish_channels_dots_calling'])(
+                                                                            img,
+                                                                            fov_subdataset,
+                                                                            processing_parameters)
+        
+    if save_steps_output:
+        fname = experiment_name + '_' + fov_subdataset.channel + '_round_' + str(fov_subdataset.round_num) + '_fov_' + str(fov_subdataset.fov_num)
+        np.save(filtered_img_path / (fname + '.npy'),img )
+        counts.to_parquet((fname + '.parquet'),index=False)
+
+    # return counts, (fov_subdataset.channel,fov_subdataset.round_num,img)
+    return counts
+
+
+def single_fov_round_processing_serial_nuclei(fov_subdataset,
+                                   analysis_parameters,
+                                   running_functions,
+                                   dark_img,
+                                   experiment_fpath,
+                                   save_steps_output=False):
+
+    """
+    Preselected the fov
+    processing_type = nuclei
+    """
+    logger = selected_logger()
+    experiment_fpath = Path(experiment_fpath)
+
+    # Path of directory where to save the intermediate results
+    filtered_img_path = experiment_fpath / 'tmp' / 'filtered_images'
+    raw_counts_path = experiment_fpath / 'tmp' / 'raw_counts'
+    
+    experiment_name = fov_subdataset.experiment_name
+    pipeline = fov_subdataset.pipeline
+    processing_type = fov_subdataset.processing_type
+    zarr_grp_name = fov_subdataset.grp_name
+    
+    raw_data_location = Path(fov_subdataset.raw_data_location)
+    parsed_raw_data_fpath = raw_data_location.parent
+
+    img = getattr(pysmFISH.preprocessing,running_functions['reference_channels_preprocessing'])(
+                                                    zarr_grp_name,
+                                                    parsed_raw_data_fpath,
+                                                    processing_parameters,
+                                                    dark_img)
+        
+    if save_steps_output:
+        fname = experiment_name + '_' + fov_subdataset.channel + '_round_' + str(fov_subdataset.round_num) + '_fov_' + str(fov_subdataset.fov_num)
+        np.save(filtered_img_path / (fname + '.npy'),img )
+
+    return img
 
 
 def fov_processing_eel_barcoded(fov,
