b4f2114717b298b220f53bdd99dd3c4958ac01f9
simone codeluppi <simone.codeluppi@gmail.com>
2021-04-13 19:29:20 +0200

    extra process

--- flows/general_flow_single_fov.py
+++ flows/general_flow_single_fov.py
@@ -102,7 +102,7 @@ pipeline_start = time.time()
 # PARAMETERS DEFINITION
 # Experiment fpath will be loaded from the scanning function
 
-experiment_fpath = '/fish/work_std/JJEXP20210323_EEL_SL005'
+experiment_fpath = '/fish/work_std/JJEXP20210318_EEL_SL007'
 
 raw_data_folder_storage_path = '/fish/rawdata'
 results_data_folder_storage_path = '/fish/results'
@@ -119,7 +119,7 @@ run_type = 're-run'
 # reparsing_from_storage 
 # no_parsing if parsing not to be performed
 
-parsing_type = 'no_parsing'
+parsing_type = 'reparsing_from_processing_folder'
 
 
 fresh_nuclei_processing = False
