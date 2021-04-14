b64ece81102b4d23a74cf9c35add150935e80620
simone codeluppi <simone.codeluppi@gmail.com>
2021-04-14 14:23:22 +0200

    missin

--- cli/cli.py
+++ cli/cli.py
@@ -1,6 +1,5 @@
 import click
 
-from cli_viz.commands import viz_utilities
 from cli_data_org.commands import data_organization_utilities
 from cli_prep.commands import setup_processing_env
 from cli_run_flows.commands import flows_runner
@@ -11,7 +10,6 @@ from cli_folder_scanner.commands import folder_monitoring_utilities
 def pysmFISH_cli():
     pass
 
-pysmFISH_cli.add_command(viz_utilities)
 pysmFISH_cli.add_command(data_organization_utilities)
 pysmFISH_cli.add_command(setup_processing_env)
 pysmFISH_cli.add_command(flows_runner)
