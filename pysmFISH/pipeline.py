606a84b247704e782276f25e34d6e5895cbb4118
simone codeluppi <simone.codeluppi@gmail.com>
2021-05-06 13:40:00 +0200

    modified imports

--- pysmFISH/pipeline.py
+++ pysmFISH/pipeline.py
@@ -4,8 +4,8 @@ import dask
 import pandas as pd
 
 from pathlib import Path
-
 from datetime import datetime
+
 from dask.distributed import Client
 from dask.base import tokenize
 from dask import dataframe as dd
@@ -14,59 +14,19 @@ from dask import delayed
 
 # Import from pysmFISH package
 
-from pysmFISH.logger_utils import selected_logger
-
-from pysmFISH.utils import create_folder_structure
-from pysmFISH.utils import collect_processing_files
-from pysmFISH.utils import sort_data_into_folders
-from pysmFISH.utils import create_dark_img
-from pysmFISH.utils import combine_filtered_images
-
-from pysmFISH.configuration_files import load_experiment_config_file
-from pysmFISH.configuration_files import load_analysis_config_file
-from pysmFISH.configuration_files import create_specific_analysis_config_file
-from pysmFISH.configuration_files import create_function_runner
-from pysmFISH.configuration_files import load_codebook
-
-from pysmFISH.io import create_empty_zarr_file
-from pysmFISH.io import consolidate_zarr_metadata
-
-from pysmFISH.data_organization import transfer_files_from_storage
-
-from pysmFISH.microscopy_file_parsers import nd2_raw_files_selector, nd2_raw_files_selector_general
-from pysmFISH.microscopy_file_parsers import nikon_nd2_autoparser_zarr, nikon_nd2_reparser_zarr
-
-from pysmFISH.processing_cluster_setup import start_processing_env
-
-from pysmFISH.data_models import Dataset
-
-
-from pysmFISH.preprocessing import load_dark_image
-
-from pysmFISH.fovs_registration import beads_based_registration
-from pysmFISH.fovs_registration import nuclei_based_registration
-
-from pysmFISH.barcodes_analysis import extract_barcodes_NN_fast
-
-from pysmFISH.fov_processing import single_fov_round_processing_eel
-from pysmFISH.fov_processing import single_fov_round_processing_serial_nuclei
-
-from pysmFISH.stitching import organize_square_tiles
-from pysmFISH.stitching import stitch_using_microscope_fov_coords_new
-
-
-from pysmFISH.io import simple_output_plotting
-from pysmFISH.io import simple_output_plotting_serial
-
-# ----------------
-# old imports
-
-
-# -----------------
-
-
-
-
+from pysmFISH import logger_utils
+from pysmFISH import utils
+from pysmFISH import configuration_files
+from pysmFISH import io
+from pysmFISH import data_organization
+from pysmFISH import microscopy_file_parsers
+from pysmFISH import processing_cluster_setup
+from pysmFISH import data_models
+from pysmFISH import preprocessing
+from pysmFISH import fovs_registration
+from pysmFISH import barcodes_analysis
+from pysmFISH import fov_processing
+from pysmFISH import stitching
 
 
 
@@ -95,14 +55,17 @@ def nice_deltastring(delta):
 
 class Pipleline():
     """
+    Generale pipeline for running barcoded eel or serial smFISH experiments
+
+
 
     """
 
-    def __init__(self, pipeline_name:str, experiment_fpath:str,
+    def __init__(self, pipeline_run_name:str, experiment_fpath:str,
                 run_type:str = 'new', parsing_type:str = 'original', **kwarg):
 
-        self.logger = selected_logger()   
-        self.pipeline_name = pipeline_name
+        self.logger = logger_utils.selected_logger()   
+        self.pipeline_run_name = pipeline_run_name
         self.experiment_fpath = Path(experiment_fpath)
         self.run_type = run_type
         self.parsing_type = parsing_type
@@ -134,13 +97,13 @@ class Pipleline():
     # ------------------------------------
 
     def load_config_files(self):
-        self.analysis_parameters = load_analysis_config_file(self.experiment_fpath)
+        self.analysis_parameters = configuration_files.load_analysis_config_file(self.experiment_fpath)
         self.parsed_raw_data_fpath = self.experiment_fpath / (self.experiment_fpath.stem + '_' + self.parsed_image_tag + '.zarr') 
     
-    def start_processing_env(self):
+    def processing_cluster_setup.start_processing_env(self):
         # Start processing environment
         self.logger.info(f'Starting processing environment')
-        self.cluster = start_processing_env(self.processing_env_config)
+        self.cluster = processing_cluster_setup.start_processing_env(self.processing_env_config)
         self.client = Client(self.cluster)
         self.logger.debug(f'Dask dashboard info {self.client.scheduler_info()['services']}')
 
@@ -149,18 +112,18 @@ class Pipleline():
         """
         explain all the steps that are run by this function
         """
-        experiment_info = load_experiment_config_file(self.experiment_fpath)
-        create_specific_analysis_config_file(self.experiment_fpath, experiment_info)
+        experiment_info = configuration_files.load_experiment_config_file(self.experiment_fpath)
+        configuration_files.create_specific_analysis_config_file(self.experiment_fpath, experiment_info)
         
         # Create empty zarr file for the parse data
-        parsed_raw_data_fpath = create_empty_zarr_file(experiment_fpath=self.experiment_fpath,
+        parsed_raw_data_fpath = io.create_empty_zarr_file(experiment_fpath=self.experiment_fpath,
                                             tag=self.parsed_image_tag)
         if parsing_type == 'original':
-            collect_processing_files(self.experiment_fpath, experiment_info)
-            sort_data_into_folders(self.experiment_fpath, experiment_info)
-            all_raw_nd2 = nd2_raw_files_selector(self.experiment_fpath)
+            utils.collect_processing_files(self.experiment_fpath, experiment_info)
+            utils.sort_data_into_folders(self.experiment_fpath, experiment_info)
+            all_raw_nd2 = microscopy_file_parsers.nd2_raw_files_selector(self.experiment_fpath)
 
-            parsing_futures = client.map(nikon_nd2_autoparser_zarr,
+            parsing_futures = client.map(microscopy_file_parsers.nikon_nd2_autoparser_zarr,
                                     all_raw_nd2,
                                     parsed_raw_data_fpath=self.parsed_raw_data_fpath,
                                     experiment_info=experiment_info)
@@ -174,21 +137,21 @@ class Pipleline():
                 raw_files_fpath = self.experiment_fpath + '/raw_data'
                 logger.info(f'raw_files_fpath {raw_files_fpath}')
             elif parsing_type == 'reparsing_from_storage':
-                transfer_files_from_storage(self.storage_experiment_fpath, self.experiment_fpath)
+                data_organization.microscopy_file_parsers.microscopy_file_parsers.nd2_raw_files_selector(self.storage_experiment_fpath, self.experiment_fpath)
                 raw_files_fpath = self.storage_experiment_fpath + '/raw_data'
             
-            all_raw_nd2 = nd2_raw_files_selector_general(folder_fpath=self.raw_files_fpath)
-            parsing_futures = client.map(nikon_nd2_reparser_zarr,
+            all_raw_nd2 = microscopy_file_parsers.nd2_raw_files_selector_general(folder_fpath=self.raw_files_fpath)
+            parsing_futures = client.map(microscopy_file_parsers.nikon_nd2_reparser_zarr,
                                     all_raw_nd2,
                                     parsed_raw_data_fpath=self.parsed_raw_data_fpath,
                                     experiment_info=experiment_info)
 
         _ = self.client.gather(parsing_futures)
-        consolidated_grp = consolidate_zarr_metadata(self.parsed_raw_data_fpath)
+        consolidated_grp = io.consolidate_zarr_metadata(self.parsed_raw_data_fpath)
 
 
     def prepare_processing_dataset(self):
-        self.ds = Dataset()
+        self.ds = data_models.Dataset()
         if self.dataset_path:
             self.ds.load_dataset(self.dataset_path)
         else:
@@ -200,7 +163,7 @@ class Pipleline():
 
     def determine_tiles_organization(self):
         self.reference_round = analysis_parameters['RegistrationReferenceHybridization']
-        self.tiles_org = organize_square_tiles(self.experiment_fpath,self.metadata,
+        self.tiles_org = stitching.organize_square_tiles(self.experiment_fpath,self.metadata,
                                     self.reference_round)
         self.tiles_org.run_tiles_organization()
         self.tile_corners_coords_pxl = self.tiles_org.tile_corners_coords_pxl
@@ -211,7 +174,7 @@ class Pipleline():
         This method create a processing graph XXXXXXXXX
 
         """
-        dark_img = load_dark_image(self.experiment_fpath)
+        dark_img = preprocessing.load_dark_image(self.experiment_fpath)
         
         # did this conversion to avoid to pass self to dask
         analysis_parameters = self.analysis_parameters
@@ -224,7 +187,7 @@ class Pipleline():
         running_functions = delayed(running_functions)
         tile_corners_coords_pxl = delayed(tile_corners_coords_pxl)
 
-        codebook = load_codebook(experiment_fpath,metadata)
+        codebook = configuration_files.load_codebook(experiment_fpath,metadata)
         codebook_df = delayed(codebook)
         
         all_processing = []
@@ -238,7 +201,7 @@ class Pipleline():
                 experiment_name = fov_subdataset.experiment_name
                 dask_delayed_name = 'filt_count_' +experiment_name + '_' + channel + \
                                 '_round_' + str(round_num) + '_fov_' +str(fov) + '-' + tokenize()
-                counts = delayed(single_fov_round_processing_eel, name=dask_delayed_name)(fov_subdataset,
+                counts = delayed(fov_processing.single_fov_round_processing_eel, name=dask_delayed_name)(fov_subdataset,
                                             analysis_parameters,
                                             running_functions,
                                             dark_img,
@@ -252,17 +215,17 @@ class Pipleline():
             
             name = 'register_' +experiment_name + '_' + channel + '_' \
                                 + '_fov_' +str(fov) + '-' + tokenize()
-            registered_counts = delayed(beads_based_registration,name=name)(all_counts_fov,
+            registered_counts = delayed(fovs_registration.beads_based_registration,name=name)(all_counts_fov,
                                                 analysis_parameters)
 
             name = 'decode_' +experiment_name + '_' + channel + '_' \
                                 + '_fov_' +str(fov) + '-' + tokenize()
-            decoded = delayed(extract_barcodes_NN_fast,name=name)(registered_counts, 
+            decoded = delayed(barcodes_analysis.extract_barcodes_NN_fast,name=name)(registered_counts, 
                                                                         analysis_parameters,codebook_df)                                                        
             
             name = 'stitch_to_mic_coords_' +experiment_name + '_' + channel + '_' \
                                 + '_fov_' +str(fov) + '-' + tokenize()  
-            stitched_coords = delayed(stitch_using_microscope_fov_coords_new,name=name)(decoded[1],tile_corners_coords_pxl)
+            stitched_coords = delayed(stitching.stitch_using_microscope_fov_coords_new,name=name)(decoded[1],tile_corners_coords_pxl)
             
             name = 'save_df_' +experiment_name + '_' + channel + '_' \
                                 + '_fov_' +str(fov) + '-' + tokenize() 
@@ -276,7 +239,7 @@ class Pipleline():
         # GENERATE OUTPUT FOR PLOTTING
         selected_Hdistance = 3 / self.metadata['barcode_length']
         stitching_selected = 'microscope_stitched'
-        simple_output_plotting(self.experiment_fpath, stitching_selected, selected_Hdistance, self.client)
+        io.simple_output_plotting(self.experiment_fpath, stitching_selected, selected_Hdistance, self.client)
         # ----------------------------------------------------------------  
 
 
@@ -286,7 +249,7 @@ class Pipleline():
 
         """
 
-        dark_img = load_dark_image(self.experiment_fpath)
+        dark_img = preprocessing.load_dark_image(self.experiment_fpath)
         
         # did this conversion to avoid to pass self to dask
         analysis_parameters = self.analysis_parameters
@@ -314,7 +277,7 @@ class Pipleline():
                     dask_delayed_name = 'filt_' +experiment_name + '_' + channel + \
                                     '_round_' + str(round_num) + '_fov_' +str(fov) + '-' + tokenize()
 
-                    out_nuclei = delayed(single_fov_round_processing_serial_nuclei,name=dask_delayed_name)(fov_subdataset,
+                    out_nuclei = delayed(fov_processing.single_fov_round_processing_serial_nuclei,name=dask_delayed_name)(fov_subdataset,
                                             analysis_parameters,
                                             running_functions,
                                             dark_img,
@@ -325,7 +288,7 @@ class Pipleline():
                 else:
                     dask_delayed_name = 'filt_count_' +experiment_name + '_' + channel + \
                                     '_round_' + str(round_num) + '_fov_' +str(fov) + '-' + tokenize()
-                    counts = delayed(single_fov_round_processing_eel,name=dask_delayed_name)(fov_subdataset,
+                    counts = delayed(fov_processing.single_fov_round_processing_eel,name=dask_delayed_name)(fov_subdataset,
                                                 analysis_parameters,
                                                 running_functions,
                                                 dark_img,
@@ -339,17 +302,17 @@ class Pipleline():
             
             name = 'create_nuclei_stack' +experiment_name + '_' + channel + '_' \
                                 + '_fov_' +str(fov) + '-' + tokenize()
-            filtered_nuclei_stack = delayed(combine_filtered_images,name=name)(all_nuclei_fov,experiment_fpath,metadata)
+            filtered_nuclei_stack = delayed(utils.combine_filtered_images,name=name)(all_nuclei_fov,experiment_fpath,metadata)
 
             name = 'register_' +experiment_name + '_' + channel + '_' \
                                 + '_fov_' +str(fov) + '-' + tokenize()
-            registered_counts = delayed(nuclei_based_registration,name=name)(all_counts_fov,
+            registered_counts = delayed(fovs_registration.nuclei_based_registration,name=name)(all_counts_fov,
                                                 filtered_nuclei_stack,
                                                 analysis_parameters)
                                                                                                 
             name = 'stitch_to_mic_coords_' +experiment_name + '_' + channel + '_' \
                                 + '_fov_' +str(fov) + '-' + tokenize()  
-            stitched_coords = delayed(stitch_using_microscope_fov_coords_new,name=name)(registered_counts,tile_corners_coords_pxl)
+            stitched_coords = delayed(stitching.stitch_using_microscope_fov_coords_new,name=name)(registered_counts,tile_corners_coords_pxl)
             
             name = 'save_df_' +experiment_name + '_' + channel + '_' \
                                 + '_fov_' +str(fov) + '-' + tokenize() 
@@ -362,7 +325,7 @@ class Pipleline():
         # # ----------------------------------------------------------------
         # GENERATE OUTPUT FOR PLOTTING
         stitching_selected = 'microscope_stitched'
-        simple_output_plotting_serial(self.experiment_fpath, stitching_selected, self.client)
+        io.simple_output_plotting_serial(self.experiment_fpath, stitching_selected, self.client)
         # ----------------------------------------------------------------  
   
 
@@ -373,9 +336,9 @@ class Pipleline():
     
     def run_all(self):
         self.load_config_files()
-        self.start_processing_env()
+        self.processing_cluster_setup.start_processing_env()
 
-        create_folder_structure(self.experiment_fpath)
+        utils.create_folder_structure(self.experiment_fpath)
         self.logger.info(f'Folder structure completed')
 
         # Run parsing only if required
@@ -386,12 +349,12 @@ class Pipleline():
         self.prepare_processing_dataset()
 
         # CREATE DARK IMAGES
-        create_dark_img(experiment_fpath,metadata)
+        utils.create_dark_img(experiment_fpath,metadata)
 
         # CREATE RUNNING FUNCTIONS
         # used to select the sets of functions to run preprocessing and dots
         # calling according to the type of processing and sample
-        running_functions = create_function_runner(experiment_fpath,metadata)
+        running_functions = configuration_files.create_function_runner(experiment_fpath,metadata)
 
         # CREATE DARK IMAGES
         self.determine_tiles_organization()
