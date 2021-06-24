b47cc43c1d9b9fb63f1a7f8e087cc65365edef80
simone codeluppi <simone.codeluppi@gmail.com>
2021-06-24 15:21:53 +0200

    adjusted command line

--- pysmFISH/cli/cli.py
+++ pysmFISH/cli/cli.py
@@ -1,7 +1,7 @@
 import click
 
 from cli_prep.commands import setup_processing_env
-from cli_folder_scanner.commands import folder_monitoring_utilities
+# from cli_folder_scanner.commands import folder_monitoring_utilities
 # from cli_run_flows.commands import flows_runner
 
 
@@ -10,7 +10,7 @@ def pysmFISH_cli():
     pass
 
 pysmFISH_cli.add_command(setup_processing_env)
-pysmFISH_cli.add_command(folder_monitoring_utilities)
+# pysmFISH_cli.add_command(folder_monitoring_utilities)
 # pysmFISH_cli.add_command(flows_runner)
 
 
