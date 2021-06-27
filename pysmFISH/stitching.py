87e6f1c1d4cc10c309ae16085a9732bde9798373
simone codeluppi <simone.codeluppi@gmail.com>
2021-06-27 19:53:21 +0200

    adjusted typo

--- pysmFISH/stitching.py
+++ pysmFISH/stitching.py
@@ -970,6 +970,7 @@ def clean_from_duplicated_dots(fov, dots_id_to_remove,experiment_fpath):
                 logger.error(f'saved {fname}')
 
                 save_name = fname.stem.split('_decoded_fov_')[0] + '_removed_df_fov_' + str(fov) + '.parquet'
+                save_name = experiment_fpath / 'results' / save_name
                 removed_df = counts_df.loc[counts_df.dot_id.isin(dots_id_to_remove), :]
                 removed_df.to_parquet(save_name,index=False)
         else:
