80a77d2cfb663b572a01f4f680c03f8250c3c8b5
simone codeluppi <simone.codeluppi@gmail.com>
2021-06-30 11:02:24 +0200

    groupby fov/channel

--- pysmFISH/pipeline.py
+++ pysmFISH/pipeline.py
@@ -581,7 +581,7 @@ class Pipeline():
                 already_done_fovs.append(fov_num)
             not_processed_fovs = set(self.grpd_fovs.groups.keys()).difference(set(already_done_fovs))
             self.data.dataset = self.data.dataset.loc[self.data.dataset.fov_num.isin(not_processed_fovs), :]
-            self.grpd_fovs = self.data.dataset.groupby('fov_num')
+            self.grpd_fovs = self.data.dataset.groupby(['fov_num','channel'])
 
 
         if self.metadata['experiment_type'] == 'eel-barcoded':
