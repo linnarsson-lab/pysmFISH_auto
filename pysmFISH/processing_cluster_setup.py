2070b2611a6b60363f9d2f94b7cdb33fd11ff221
simone codeluppi <simone.codeluppi@gmail.com>
2021-10-06 16:34:23 +0200

    adjusted typo

--- pysmFISH/processing_cluster_setup.py
+++ pysmFISH/processing_cluster_setup.py
@@ -104,7 +104,7 @@ def unmanaged_cluster_setup(htcondor_cluster_setup:Dict):
     all_addresses = [scheduler_address] + workers_addresses_list
     
     worker_options = {"nprocs":nprocs,
-                     "cores":cores,
+                     "ncores":cores,
                      "memory_limit":memory,
                      "nthreads":nthreads,
                      "local_directory":local_directory}
