101498ba42a095a67bb9c1c6496e448ff8676ba0
simone codeluppi <simone.codeluppi@gmail.com>
2021-12-01 10:14:00 +0100

    adjusted reference cli_prep

--- pysmFISH/cli/cli.py
+++ pysmFISH/cli/cli.py
@@ -1,6 +1,6 @@
 import click
 
-from cli_prep.commands import setup_processing_env
+from .cli_prep.commands import setup_processing_env
 # from cli_folder_scanner.commands import folder_monitoring_utilities
 # from cli_run_flows.commands import flows_runner
 
