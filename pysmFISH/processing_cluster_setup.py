062bcf8067fd7f521b6c56c64af11a3263f4566b
simone codeluppi <simone.codeluppi@gmail.com>
2021-12-05 06:15:23 +0100

    changed known hosts parameter

--- pysmFISH/processing_cluster_setup.py
+++ pysmFISH/processing_cluster_setup.py
@@ -111,12 +111,13 @@ def unmanaged_cluster_setup(htcondor_cluster_setup:Dict):
     
     cluster = SSHCluster(
         all_addresses,
-        connect_options={"known_hosts": None},
+        connect_options={"known_hosts": workers_addresses_list},
         worker_options=worker_options,
         scheduler_options={"port": scheduler_port, 
                        "dashboard_address":dashboard_port}
         )
 
+    logger.info(f'cluster properties: htcondor_cluster_setup {htcondor_cluster_setup} ')
     return cluster
 
 
