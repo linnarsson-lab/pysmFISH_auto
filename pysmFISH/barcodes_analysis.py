04c08225ee476ab874f6e7578080f2da73a72605
simone <simone.codeluppi@gmail.com>
2023-02-06 11:16:35 +0100

    m

--- pysmFISH/barcodes_analysis.py
+++ pysmFISH/barcodes_analysis.py
@@ -485,7 +485,7 @@ def extract_barcodes_NN_fast_multicolor_recollect(registered_counts_df: pd.DataF
         return registered_counts_df, all_decoded_dots_df
         
     else:
-        for recollect in [1,3]:
+        for recollect in [2,3]:
             barcoded_round_rounds = []
             old_recollect = recollect
             barcodes_extraction_resolution_ = recollect
